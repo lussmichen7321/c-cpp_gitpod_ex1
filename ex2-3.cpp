caaeb009584306d8840b44aad2aaa62e9ae19cc9
lussmichen7321 <92796880+lussmichen7321@users.noreply.github.com>
2022-05-03 21:30:28 +0800

    Create ex2-3.cpp

---
+++ ex2-3.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,b,c,d;
+    printf("please input two numbers:");
+    scanf("%d %d",&a,&b);
+    c=a*a;
+    d=b*b;
+    printf("%d",c+d);
+
+    return 0;
+}
